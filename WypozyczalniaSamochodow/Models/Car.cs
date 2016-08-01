2b0b1daea09c6165197f83e74ec7df3fb8e1543e
kuba <jakubchudy71@gmail.com>
2016-08-01 15:47:35 +0200

    init

---
+++ WypozyczalniaSamochodow/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace WypozyczalniaSamochodow.Models
+{
+    public class Car
+    {
+        public int ID { get; set; }
+        public string license { get; set; }
+        public DateTime registrationDate { get; set; }
+        public decimal price { get; set; }
+    }
+
+    public class CarRentalDB : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+    }
+}
---
+++ WypozyczalniaSamochodow/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WypozyczalniaSamochodow.Startup))]
+namespace WypozyczalniaSamochodow
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
