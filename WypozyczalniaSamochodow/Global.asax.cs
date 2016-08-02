94e5a24fe93277ea58f04d2bca0aac009e2f2550
kuba <jakubchudy71@gmail.com>
2016-08-02 20:46:09 +0200

    no message

--- WypozyczalniaSamochodow/Global.asax.cs
+++ WypozyczalniaSamochodow/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.ApplicationServices;
 
 namespace WypozyczalniaSamochodow
 {
@@ -14,7 +15,6 @@ namespace WypozyczalniaSamochodow
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
---
+++ WypozyczalniaSamochodow/Helpers/DBConnection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WypozyczalniaSamochodow.Helpers
+{
+    public static class DBConnection
+    {
+        private static string strConnnection = ConfigurationManager.ConnectionStrings["CarRentalDB"].ConnectionString.ToString();
+
+        public static DataSet ExecuteQuery(string query)
+        {
+            SqlConnection conn = new SqlConnection(strConnnection);
+            SqlDataAdapter da = new SqlDataAdapter();
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = query;
+            da.SelectCommand = cmd;
+            DataSet ds = new DataSet();
+
+            conn.Open();
+            da.Fill(ds);
+            conn.Close();
+
+            return ds;
+        }
+    }
+}
